e71978408f82a32dec5aa950d290a108b96bd69b
Nami19 <bonnamae.pitogo@dbtc-cebu.edu.ph>
2026-08-04 14:34:11 +0800

    Remove ITE404A

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,20 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
+import { Footer } from './footer/footer';
+import { ProfileCard } from './profile-card/profile-card';
+import { AnnouncementCard } from './announcement-card/announcement-card';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+  Header,
+  ProfileCard,
+  AnnouncementCard,
+  Footer
+],  
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('Bonna Mae Pitogo');
-}
+export class App {}
